89553a8c4a698658e3f7809e1473de9f460b97aa
Jeff Irion <jefflirion@users.noreply.github.com>
2019-06-15 10:09:06 -0700

    Remove get_ha_components.sh

--- get_ha_components.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# input_number
-rm -rf input_number
-mkdir input_number
-wget -O input_number/__init__.py https://raw.githubusercontent.com/home-assistant/home-assistant/dev/homeassistant/components/input_number/__init__.py
-wget -O input_number/manifest.json https://raw.githubusercontent.com/home-assistant/home-assistant/dev/homeassistant/components/input_number/manifest.json
-wget -O input_number/services.yaml https://raw.githubusercontent.com/home-assistant/home-assistant/dev/homeassistant/components/input_number/services.yaml
